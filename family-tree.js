d37f82940f1bbf951ed0e30ebb9263de2c92905c
Christine Su <christine.hui.su@gmail.com>
2021-01-16 19:16:26 -0500

    first commit

--- family-tree.js
+++ family-tree.js
@@ -1,5 +1,28 @@
 class FamilyTree {
-  constructor () {}
+  constructor (name) {
+    if (name === undefined) throw 'need to give a name'
+    this.value = name
+  }
+
+// `familySize`: Returns this size of **this parent and their children.**
+familySize () {
+  return 0;
+}
+
+// `findMember`: Given a `name`, it will return the `node` where that member exists. Otherwise, returns undefined.
+
+findMember(name) {
+  return obj;
+}
+// `log`: Logs out a specific structure (exampled below) of the family from this point down.
+
+log() {
+  let str = ''
+  return str
+}
+// `insert`: Inserts a child at this node.
+
+
 }
 
 module.exports = FamilyTree;
